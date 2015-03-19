6ea975f65eb52ab3a45a5fe3903465837ae9885e
MKS-Student <mks.student@hackreactor.com>
2015-03-19 11:24:27 -0700

    MyTunes meets basic requirements. (no css)

--- client/views/SongQueueView.js
+++ client/views/SongQueueView.js
@@ -6,14 +6,25 @@ var SongQueueView = Backbone.View.extend({
   initialize: function() {
     this.listenTo(this.model, 'ended', function() {
       this.model.donePlaying();
-      //this.$el
+      this.$el.find('tr').first().remove()
     }, this);
-    this.listenTo(this.model, 'dequeue', function() {
-      this.model.shift();
+
+    this.listenTo(this.model, 'dequeue', function(song) {
+      this.$el.find('tr:contains(' + song.get("artist") + '):contains(' + song.get("title") + ')')
+              .remove();
+      if(this.model.at(0)===song) {
+        $('audio')[0].pause();
+        $('audio')[0].currentTime = 0;
+        this.model.donePlaying();
+      } else {
+        this.model.remove(song);
+      }
     }, this);
+
     this.listenTo(this.model, 'add', function() {
       this.$el.append(new SongQueueEntryView({model: this.model.at(this.model.length-1)}).render());
     }, this);
+    
     this.$el.html('<th>Queue</th>');
     this.render();
   },
@@ -22,9 +33,4 @@ var SongQueueView = Backbone.View.extend({
     return this.$el;
   },
 
-  // listen for ended, dequeue, add
-
-
 });
-
-//<th>Queue</th>
